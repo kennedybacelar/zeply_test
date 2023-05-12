1702fb2a9b84fdc4924c475a3c1c9156f9665242
Kennedy <kennedybacelar@yahoo.com.br>
2023-05-12 14:54:16 -0300

    returning first btc address

--- api/app/core.py
+++ api/app/core.py
@@ -1,14 +1,14 @@
 from pycoin.key import Key
-from pycoin.networks import BitcoinMainnet, EthereumMainnet
+from pycoin.networks.registry import network_for_netcode
 from models.models import Coin
 
 coin_vs_network = {
-    Coin.BTC: BitcoinMainnet,
-    Coin.ETH: EthereumMainnet,
+    Coin.BTC: network_for_netcode("XTN"),  # XTN represents the BTC Testnet network
 }
 
 
 def generate_address(coin: Coin):
     coin_network = coin_vs_network[coin]
-    key = Key.generate(network=coin_network)
+    key = coin_network.keys.private(secret_exponent=1)
+
     return key.address()
